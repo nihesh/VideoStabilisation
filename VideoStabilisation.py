a0e2564a39b1f972fa249018603a31252b54beb6
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2019-12-02 07:46:59 +0530

    .

--- VideoStabilisation.py
+++ VideoStabilisation.py
@@ -4,9 +4,12 @@
 import cv2
 import numpy as np
 from utils import homography_estimator, moving_average_filter
-from Args import FPS, MOVING_AVERAGE_LENGTH, MAX_EPOCHS, CROP 
+from Args import FPS, MOVING_AVERAGE_LENGTH, MAX_EPOCHS, CROP, FILE 
 from homography import myhomography
 import scipy.misc
+from matplotlib import pyplot as plt
+
+DIM = None
 
 class VideoStabiliser:
 
@@ -22,40 +25,85 @@ class VideoStabiliser:
 
 		self.motion_data = []
 
+	def get_params(self, homography):
+
+		x_movement = homography[0][2]
+		y_movement = homography[1][2]
+
+		angle = np.arctan2(homography[1][0], homography[0][0])
+
+		return [ x_movement, y_movement, angle ]
+
+	def restoreParams(self, vector):
+
+		homography = np.zeros((3, 3))
+		homography[1][1] = homography[0][0] = np.cos(vector[2])
+		homography[0][1] = -np.sin(vector[2])
+		homography[1][0] = -homography[0][1]
+		homography[0][2] = vector[0]
+		homography[1][2] = vector[1]
+		homography[2][2] = 1
+
+		return homography
+
 	def learn_motion(self):
+
+		global DIM
 
 		it = 0
 
 		while(self.status and it < MAX_EPOCHS):
 
 			self.status, cur_frame = self.capture.read()
+			DIM = cur_frame.shape
 
 			if(not self.status):
 				break
 			
-			homography = np.round_(myhomography(self.prev_frame, cur_frame))
-			# homography = homography_estimator(self.prev_frame,cur_frame)
-			self.motion_data.append(homography)
-
-			self.prev_frame = cur_frame
-			it += 1
-
-		self.motion_data = np.asarray(self.motion_data)
+			homography = homography_estimator(self.prev_frame,cur_frame)
+			self.motion_data.append(self.get_params(homography))
 
 		self.capture.release()
 		cv2.destroyAllWindows()
 
 	def smoothen(self):
 
+		self.before_smoothing = []
+
+		for i in range(self.motion_data.shape[0]):
+			self.before_smoothing.append(np.linalg.norm(self.motion_data[i]))
+
+		plt.clf()
+		plt.plot(self.before_smoothing)
+		plt.title("Homography magnitude vs frame")
+		plt.xlabel("Frame number")
+		plt.ylabel("Magnitude")
+		plt.savefig("./output/AffineCurveBeforeSmoothening.jpg")
+
 		self.motion_data = moving_average_filter(self.motion_data, averaging_length = MOVING_AVERAGE_LENGTH)
 
+		self.after_smoothing = []
+
+		for i in range(self.motion_data.shape[0]):
+			self.after_smoothing.append(np.linalg.norm(self.motion_data[i]))
+
+
+		plt.clf()
+		plt.plot(self.after_smoothing)
+		plt.title("Homography magnitude vs frame")
+		plt.xlabel("Frame number")
+		plt.ylabel("Magnitude")
+		plt.savefig("./output/AffineCurveAfterSmoothening.jpg")
+
 	def saveVideo(self):
+
+		global DIM
 
 		n = self.motion_data.shape[0]
 		capture = cv2.VideoCapture(self.src)
 		# fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
 		fourcc = cv2.VideoWriter_fourcc(*'XVID')
-		out = cv2.VideoWriter('./output.avi', fourcc, FPS, (2560, 720))
+		out = cv2.VideoWriter('./output/' + FILE + ".avi", fourcc, FPS, (DIM[1] * 2, DIM[0]))
 
 		for i in range(n):
 
@@ -64,7 +112,7 @@ class VideoStabiliser:
 			if(not status):
 				break
 
-			output = cv2.warpAffine(frame, self.motion_data[i][:-1], (self.w, self.h))
+			output = cv2.warpAffine(frame, self.restoreParams(self.motion_data[i])[:-1], (self.w, self.h))
 			output = output[CROP:-CROP, CROP:-CROP]
 			output = scipy.misc.imresize(output, (self.h, self.w), interp = "bilinear", mode = None)
 
@@ -78,7 +126,11 @@ class VideoStabiliser:
 
 if(__name__ == "__main__"):
 
+<<<<<<< HEAD
 	video_stabiliser = VideoStabiliser("samples/video.mp4")
+=======
+	video_stabiliser = VideoStabiliser("./samples/" + FILE + ".mp4")
+>>>>>>> ffa50d6d6e4446f0ca4864cbb29d57bf3a80437c
 	video_stabiliser.learn_motion()
 	video_stabiliser.smoothen()
 	video_stabiliser.saveVideo()
