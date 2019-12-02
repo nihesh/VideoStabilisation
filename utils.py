582cc3538bd5e158a50e9dc83a1c1a6739fcfb6c
Mrinal Paliwal <mrinal16164@iiitd.ac.in>
2019-12-02 07:43:48 +0530

    Update utils

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import cv2
+import math
 from Args import NUM_CORRESPONDENCES
 
 def moving_average_filter(data, averaging_length = 3):
@@ -71,6 +72,65 @@ def homography_estimator(prev_frame, cur_frame):
 	# visualise_homography(prev_frame, homography)
 
 	return homography
+
+# Function to display image
+def display(image):
+    cv2.imshow("image",image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+# Function to pad image for convolution
+def pad(image, kernelX, kernelY):
+    imageX, imageY = image.shape    
+    kernelXmid, kernelYmid = kernelX//2, kernelY//2
+    image_padded = np.zeros((imageX+kernelX-1,imageY+kernelY-1),dtype=np.uint8)
+    #padding the center
+    image_padded[kernelXmid:imageX+kernelXmid,kernelYmid:imageY+kernelYmid] = image 
+    #paddng top
+    image_padded[0:kernelXmid, kernelYmid:imageY+kernelYmid] = image[0:kernelXmid,:]
+    #padding bottom
+    image_padded[imageX+kernelXmid:, kernelYmid:imageY+kernelYmid] = image[imageX-kernelXmid:,:]
+    #padding left
+    image_padded[:, 0:kernelYmid] = image_padded[:,kernelYmid:2*kernelYmid]
+    #padding right
+    image_padded[:, imageY+kernelYmid:] = image_padded[:,imageY:imageY+kernelYmid]
+    return image_padded
+
+# Function to perform convolution
+def conv(image, kernel):
+    kernelX, kernelY = kernel.shape
+    kernelXmid, kernelYmid = kernelX//2, kernelY//2
+    imageX, imageY = image.shape
+    new_image = np.zeros(image.shape,dtype=np.uint8)
+    image_padded = pad(image, kernelX, kernelY)
+    for i in range(kernelXmid, imageX+kernelXmid):
+        for j in range(kernelYmid,imageY+kernelYmid):
+            new_image[i-kernelXmid][j-kernelYmid] = np.sum(image_padded[i-kernelXmid:i+kernelXmid+1, j-kernelYmid:j+kernelYmid+1]*kernel)
+    return new_image
+
+# Function to general a gaussian kernel
+def gaussian(kernel, sigma):
+    K = np.zeros((kernel, kernel))
+    kernelX, kernelY = K.shape
+    kernelXmid, kernelYmid = kernelX//2, kernelY//2
+    weight = 0
+    for i in range(kernelX):
+        for j in range(kernelY):
+            # K[i][j] = int(100*math.exp(-1*((i-kernelXmid)**2 + (j-kernelYmid)**2)/(2*(sigma**2))))
+            K[i][j] = math.exp(-1*((i-kernelXmid)**2 + (j-kernelYmid)**2)/(2*(sigma**2)))
+            weight+= K[i][j]
+    return K, weight
+
+def downsample(image, scale):
+    return np.array(image[::scale,::scale])
+
+def rotate90(image):
+    rotated = np.zeros(image.shape[::-1], dtype=type(image[0,0]))
+    for i in range(image.shape[1]):
+        rotated[i,:] = image[:,i].T[::-1]
+    return rotated
+
+
 
 if(__name__ == "__main__"):
 
